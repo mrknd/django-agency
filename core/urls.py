2e47cb3c8cc8f8cbf86abac13f4f61ae602fb245
mrkn <markiian.design@gmail.com>
2026-03-25 17:42:16 +0300

    switch to DATABASE_URL

--- core/urls.py
+++ core/urls.py
@@ -15,5 +15,4 @@ urlpatterns = [
     path('blog/<slug:slug>/', blog_detail, name='blog_detail')
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
